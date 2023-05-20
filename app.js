ec5781073c63c8f651c07d60d5ddc8f89ef3dbf7
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-05-20 17:47:47 +0900

    update

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", function (req, res) {
 
@@ -13,13 +17,15 @@ app.get("/", function (req, res) {
         day: "numeric",
         month: "long"
     };
-
     var day = today.toLocaleDateString("en-US", options);
+    res.render("list", { day: day, todo: items });
+});
 
+app.post("/", function (req, res) {
+    var item = req.body.todo;
+    items.push(item);
 
-    res.render("list", {
-        day: day
-    });
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
